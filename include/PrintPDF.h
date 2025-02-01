84106ccd7e5b5b61aeb0a5e7cbf8ba793579c96c
zihasyu <1045839067@qq.com>
2025-02-01 15:32:48 +0800

    fixed 61_90 bug

--- include/PrintPDF.h
+++ include/PrintPDF.h
@@ -15,13 +15,14 @@ namespace fs = std::filesystem;
 const float mm_to_points = 72.0 / 25.4;                      // 毫米到点的转换系数
 const float page_width = 210.0 * mm_to_points;               // A4 width in points
 const float page_height = 297.0 * mm_to_points;              // A4 height in points
-const float margin = 10 * mm_to_points;                      // 页边距
+const float margin = 5 * mm_to_points;                       // 页边距
 const int images_per_row = 3;                                // 每行图片数量
 const int images_per_col = 3;                                // 每列图片数量
 const int images_per_page = images_per_row * images_per_col; // 每页图片数量
 const float image_width = 59.0 * mm_to_points;               // 每个图片的宽度 (5.9 cm)
 const float image_height = 86.0 * mm_to_points;              // 每个图片的高度 (8.6 cm)
-
+// const float image_width = (59.0 + 59.0 * 4.0 / 86.0) * mm_to_points; // 每个图片的宽度 (5.9 cm)
+// const float image_height = (86.0 + 4.0) * mm_to_points;
 void error_handler(HPDF_STATUS error_no, HPDF_STATUS detail_no, void *user_data);
 std::vector<std::wstring> load_image_paths_from_ydk(const std::wstring &ydk_path, const std::wstring &image_dir);
 std::wstring string_to_wstring(const std::string &str);
